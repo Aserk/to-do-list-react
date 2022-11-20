4b4a633870be6ddb965d61cabfe35b2807e21b41
Rafał Jabłoński <asserk.codes@gmail.com>
2022-11-20 12:04:20 +0100

    removed <div className="section__body">

--- src/Section/index.js
+++ src/Section/index.js
@@ -6,9 +6,7 @@ const Section = ({ title, body, extraHeaderContent }) => (
             <h2 className="section__title ">{title}</h2>
             {extraHeaderContent}
         </div>
-        <div className="section__body">
-            {body}
-        </div>
+        {body}
     </section>
 );
 
