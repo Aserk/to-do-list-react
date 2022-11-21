f7bba32e856bfb4ba9d0d2da0221655ced01c5df
Rafał Jabłoński <asserk.codes@gmail.com>
2022-11-21 23:42:05 +0100

    adding new task interaction

--- src/App.js
+++ src/App.js
@@ -36,6 +36,17 @@ function App() {
     })));
   };
 
+  const addNewTask = (content) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
+      },
+    ]);
+  };
+
   return (
     <Container>
       <header>
@@ -43,7 +54,7 @@ function App() {
       </header>
       <Section
         title="Dodaj nowe zadanie"
-        body={<Form />}
+        body={<Form addNewTask={addNewTask} />}
       />
 
       <Section
