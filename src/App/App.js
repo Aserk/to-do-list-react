333c9679bb9607f6c52e209e728007efa3efa4af
Rafał Jabłoński <asserk.codes@gmail.com>
2023-08-27 20:24:39 +0200

    moved "navigation" to "common"

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,7 @@ import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import TasksPage from "./features/tasks/TasksPage";
 import TaskPage from "./features/tasks/TaskPage";
 import AuthorPage from "./features/author/AuthorPage";
-import Navigation from "./features/Navigation";
+import Navigation from "./common/Navigation";
 import { toAuthor, toTask, toTasks } from "./routes";
 
 
--- src/App/features/tasks/getExampleTasks.js
+++ src/App/features/tasks/getExampleTasks.js
@@ -1,5 +1,7 @@
+const exampleTasks = "/todo-list-react/exampleTasks.json";
+
 export const getExampleTasks = async () => {
-    const response = await fetch("/todo-list-react/exampleTasks.json");
+    const response = await fetch(exampleTasks);
 
     if (!response.ok) {
         new Error(response.statusText);
